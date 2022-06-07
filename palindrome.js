4ea835f2127a31b24d628fde7c2b53ca3bf37b9f
camronthecoder <106614872+camronthecoder@users.noreply.github.com>
2022-06-07 10:42:57 -0400

    first draft

---
+++ palindrome.js
@@ -0,0 +1,20 @@
+let frontSpelledWord = []
+let backSpelledWord = []
+
+
+function palindrome(word){
+
+for (let j = 0; j<=word.length-1; j++){
+    frontSpelledWord.push(word[j])}
+for (let i = word.length-1; i>= 0; i--){
+    backSpelledWord.push(word[i])}
+
+
+if (frontSpelledWord[k] === backSpelledWord[k]){
+        console.log("Yes,", word, "is a palindrome.")}
+else {console.log ("No,", word, "is not a palindrome.")}
+
+console.log("Front spelling:",frontSpelledWord)
+console.log("Back Spelling:",backSpelledWord)}
+
+palindrome("reviver");
